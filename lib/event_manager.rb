e47d08a02b45bb441a0c84de9866d709cfdad2c8
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-09-20 18:20:53 -0400

    added ERB templates

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require "csv"
 require 'sunlight/congress'
+require 'erb'
 Sunlight::Congress.api_key = 'e179a6973728c4dd3fb1204283aaccb5'
 
 
@@ -11,27 +12,35 @@ end
 
 def legislators_by_zipcode(zipcode)
   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
+end
+
+def save_thank_you_letters(id, form_letter)
+  Dir.mkdir("output") unless Dir.exists? "output"
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
   end
-  legislator_names.join(", ")
 end
 
 contents = CSV.open "event_attendees.csv",
   headers: true,
   header_converters: :symbol
 
-template_letter = File.read "form_letter.html"
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+
   legislators = legislators_by_zipcode(zipcode)
 
-  personal_letter = template_letter.gsub('FIRST_NAME', name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
+  form_letter = erb_template.result(binding)
 
-  puts personal_letter
+  save_thank_you_letters(id, form_letter)
 
 end
 
